239a08a42f8c4d96d9c87f7bdb49ad08afbf2fc2
Bill Agee <billagee@gmail.com>
2024-10-01 11:43:06 -0700

    Clean up main.tf and use a private s3 bucket

--- infra/main.tf
+++ infra/main.tf
@@ -1,75 +1,141 @@
-provider "aws" {
-  region = "us-east-1"
+####
+# NOTE: Prerequisites/resources not defined in this project:
+# - the likewise.org hosted zone in route53
+# - a valid SSL cert in ACM for blog-dev.likewise.org
+#
+# These will be pulled in with data sources below.
+#
+# See https://www.youtube.com/watch?v=U9n6N56neuo for a good overview of the setup.
+####
+
+locals {
+  s3_bucket_name     = "blog-dev.likewise.org"
+  domain             = "blog-dev.likewise.org"
+  route53_zone_name  = "likewise.org."
 }
 
-resource "aws_s3_bucket" "blogdev_likewise_org" {
-  bucket = "blogdev.likewise.org"
+##
+#### DATA SOURCES
+##
+
+data "aws_route53_zone" "main" {
+  name         = local.route53_zone_name
+  private_zone = false
+}
+
+data "aws_acm_certificate" "main" {
+  domain   = local.domain
+  statuses = ["ISSUED"]
+}
+
+##
+#### S3 CONFIGURATION
+##
+
+resource "aws_s3_bucket" "main" {
+  bucket = local.s3_bucket_name
 
   tags = {
-    Name        = "blogdev.likewise.org static site bucket"
+    Name        = "${local.domain} static site bucket"
     Environment = "Dev"
   }
 }
 
-resource "aws_s3_bucket_ownership_controls" "ownership-controls" {
-  bucket = aws_s3_bucket.blogdev_likewise_org.id
-  rule {
-    object_ownership = "BucketOwnerPreferred"
+resource "aws_s3_bucket_policy" "main" {
+  bucket = aws_s3_bucket.main.id
+  policy = data.aws_iam_policy_document.cloudfront_oac_access.json
+}
+
+##
+#### CLOUDFRONT CONFIGURATION
+##
+
+resource aws_cloudfront_distribution "main" {
+  enabled = true
+  aliases = [local.domain]
+  default_root_object = "index.html"
+  is_ipv6_enabled     = true
+  wait_for_deployment = true
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    # CachingOptimized managed policy ID from
+    # https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/using-managed-cache-policies.html#managed-cache-caching-optimized
+    cache_policy_id = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+    target_origin_id = aws_s3_bucket.main.bucket
+    viewer_protocol_policy = "redirect-to-https"
+
+    /*
+    forwarded_values {
+      query_string = false
+    
+      cookies {
+        forward = "none"
+      }
+    }
+    */
+  }
+
+  origin {
+    domain_name = aws_s3_bucket.main.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.main.id
+    origin_id   = aws_s3_bucket.main.bucket # Same value as .id
+  }
+
+  comment             = local.s3_bucket_name
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = data.aws_acm_certificate.main.arn
+    minimum_protocol_version = "TLSv1.2_2021"
+    ssl_support_method = "sni-only"
+  }
+
+  tags = {
+    Environment = "Dev"
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "public-access-block" {
-  bucket = aws_s3_bucket.blogdev_likewise_org.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+resource aws_cloudfront_origin_access_control "main" {
+  name = "${local.s3_bucket_name}-oac"
+  origin_access_control_origin_type = "s3"
+  signing_behavior = "always"
+  signing_protocol = "sigv4"
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  depends_on = [
-    aws_s3_bucket_ownership_controls.ownership-controls,
-    aws_s3_bucket_public_access_block.public-access-block,
-  ]
-
-  bucket = aws_s3_bucket.blogdev_likewise_org.id
-  acl    = "public-read"
-}
-
-resource "aws_s3_bucket_website_configuration" "blogdev_likewise_org_website_config" {
-  bucket = aws_s3_bucket.blogdev_likewise_org.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
+data "aws_iam_policy_document" "cloudfront_oac_access" {
+  statement {
+    principals {
+      identifiers = ["cloudfront.amazonaws.com"]
+      type        = "Service"
+    }
+    actions = [
+      "s3:GetObject",
+    ]
+    resources = ["${aws_s3_bucket.main.arn}/*"]
+    
+    condition {
+      test = "StringEquals"
+      values = [aws_cloudfront_distribution.main.arn]
+      variable = "AWS:SourceArn"
+    }
   }
 }
 
-# Data source to fetch the hosted zone details not defined in TF
-data "aws_route53_zone" "likewise_zone" {
-  name         = "likewise.org."
-  private_zone = false
-}
-
-resource "aws_route53_record" "blogdev-a-record" {
-  zone_id = data.aws_route53_zone.likewise_zone.zone_id
-  name    = "blogdev.likewise.org"
+#### ROUTE53 CONFIGURATION
+resource "aws_route53_record" "main" {
+  name    = local.domain
   type    = "A"
+  zone_id = data.aws_route53_zone.main.zone_id
 
   alias {
-    # Has a trailing dot which breaks the DNS record:
-    #name                   = aws_s3_bucket_website_confguration.blogdev_likewise_org_website_config.website_endpoint
-    name                   = "s3-website-us-east-1.amazonaws.com"
-    zone_id                = aws_s3_bucket.blogdev_likewise_org.hosted_zone_id
+    name                   = aws_cloudfront_distribution.main.domain_name
+    zone_id                = aws_cloudfront_distribution.main.hosted_zone_id
     evaluate_target_health = true
   }
-}
-
-output "s3_bucket_website_url" {
-  #value = aws_s3_bucket.mysitebucket.website_endpoint
-  value = aws_s3_bucket_website_configuration.blogdev_likewise_org_website_config.website_endpoint
 }
